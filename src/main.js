24c134060bcce1abd6a4df31052added1bd14177
Gunbam27 <mingmi_27@naver.com>
2022-11-22 23:01:19 +0900

    11.22

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+gsap.registerPlugin(ScrollTrigger);
+
 const UI_left = document.querySelector('.ui_left');
 const svg_left = document.querySelector('.left_section');
 const UI_right = document.querySelector('.ui_left');
@@ -12,11 +14,26 @@ UI_left.addEventListener('mouseleave',()=>{
     svg_right.classList.remove('right_leave')
 })
 
+const elm = document.querySelectorAll('section');
+const elmCount = elm.length;
+console.log(elmCount);
+const wrap_background = document.querySelector('.wrap_background')
+window.addEventListener("load", ()=> {
+    elm.forEach(function (item,index){
+        item.addEventListener('wheel',(e)=>{
+            e.preventDefault();
+            let delta = 0;
+            let moveTop = window.scrollY;
+            let elmSelector = elm[index];
 
-// const svgMain = document.querySelector(".svg_main")
-// const mainPaths = svgMain.querySelectorAll("path");
-
-// mainPaths.forEach((path,i)=>{
-//     const length= path.getTotalLength();
-//     path.style.setProperty('--le')
-// })
+            if(delta<0){
+                if(elmSelector !== elmCount-1){
+                    moveTop = window.pageYOffset + elmSelector.nextElementSibling.getBoundingClientRect().top;
+                }
+            }
+            const body = document.querySelector('html');
+            window.scrollTo({top:moveTop, left:0, behavior:'smooth'});
+        })
+    })
+});
+    
